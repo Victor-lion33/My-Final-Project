0a05397fb60393110d2c4c3333d79f3909413131
Victor-lion33 <victorchidubem95@gmail.com>
2025-10-24 13:09:18 +0100

    made changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 
 //middleware
 app.use(cors({
-    origin: "http://localhost:5174", //frontend origin
+    origin: "http://localhost:5173" || "http://localhost:5174" //frontend origin
 }));
 app.use(express.json());//to parse JSON bodies
 app.use(ratelimiter);
