cce76479140738ccad4e1230c630c38a0339e1f8
Victor-lion33 <victorchidubem95@gmail.com>
2025-10-24 16:38:16 +0100

    more works

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors({
   origin: [
     "http://localhost:5173", // your frontend URL for development
-    "https://lion-note-app.vercel.app/" // your deployed frontend URL
+    "https://lion-note-app.vercel.app" // your deployed frontend URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
