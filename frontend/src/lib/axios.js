21fd7c076e3c9a13d98116d1f3d3fc459ad123a6
Victor-lion33 <victorchidubem95@gmail.com>
2025-10-27 22:52:58 +0100

    cleanup: removed commented fallback URLs in axios config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL // || "http://localhost:8000/api" || "https://lion-note-app-backend.vercel.app/api"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export default axiosInstance;
-
-// VITE_API_URL=https://lion-note-app-backend.vercel.app/api
-// # VITE_API_URL=http://localhost:8000/api
