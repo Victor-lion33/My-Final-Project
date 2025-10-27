37eeeb6bace1fb2fb0f634360bf041256957e1a4
Victor-lion33 <victorchidubem95@gmail.com>
2025-10-27 15:59:35 +0100

    Updated API URL for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api", //"https://lion-note-app-backend.vercel.app/api"
+    baseURL: import.meta.env.VITE_API_URL // || "http://localhost:8000/api" || "https://lion-note-app-backend.vercel.app/api"
 });
 
 export default axiosInstance;
